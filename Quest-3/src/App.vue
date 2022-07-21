6cadca38b20f8505b0e0cd721d2652ca68c84021
Muhammad Muhsin <muhmuhsin16@gmail.com>
2022-07-21 17:52:05 +0700

    Quest-3

--- Quest-3/src/App.vue
+++ Quest-3/src/App.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Input from "./components/InputPassword.vue";
 </script>
 
 <template>
   <div class="flex justify-center">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Input />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
---
+++ Quest-3/src/components/InputPassword.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { createApp, ref } from "vue";
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+
+const switchVisibility = () => {
+  const passwordField = document.getElementById("password");
+  debugger;
+  console.log(passwordField);
+  if (passwordField.getAttribute("type") === "password")
+    passwordField.setAttribute("type", "text");
+  else passwordField.setAttribute("type", "password");
+};
+</script>
+
+<template>
+  <div>
+    <input
+      type="password"
+      id="password"
+    >
+    <button @click="switchVisibility()">show / hide</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
